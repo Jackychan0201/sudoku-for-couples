57f5e7c7208b36e45bc0061e6c58ca0bb544c03c
Jackychan0201 <150186719+Jackychan0201@users.noreply.github.com>
2025-11-04 00:59:57 +0100

    fix: cors v3

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://sudoku-for-couples.vercel.app'],
+    origin: ['https://sudoku-for-couples.vercel.app', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
